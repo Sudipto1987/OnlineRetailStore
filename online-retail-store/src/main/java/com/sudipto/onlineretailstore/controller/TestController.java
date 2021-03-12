73666f93a12ab221e7cbe23d09b38e2c36017a8a
Sudipto <sudipto.roy.babun.004@gmail.com>
2021-03-12 19:08:54 +0800

    Initial commit.

---
+++ online-retail-store/src/main/java/com/sudipto/onlineretailstore/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.sudipto.onlineretailstore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+	
+	
+
+	@GetMapping("/users")
+	public String findAll() {
+		return "Hi Babun";
+	}
+
+
+}
